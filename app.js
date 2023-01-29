1a4c244fe59254ac840d2aae4c0d073b283e8e2b
Sualeh Siddiqui <sualehmsiddiqui@gmail.com>
2023-01-29 20:54:24 +0500

    adding-js-assignment-2-chap-2
    adding-js-assignment-2-chap-2

---
+++ app.js
@@ -0,0 +1,46 @@
+// Chap #2
+
+// Question #1
+// var username;
+
+// Question #2
+// var myName = ("M.Sualeh Siddiqui");
+
+// Question #3
+// var message =('"Hello World"');
+
+// window.alert (message);
+
+// Question #4
+// var name = ("M.Sualeh Siddiqui");
+// var age = ("16 years old");
+// var course = ("Web & Mobile App Developement");
+
+// window.alert (name);
+// window.alert (age);
+// window.alert (course);
+
+// Question #5
+// var piz = (" Pizza \n Pizz \n Piz \n Pi \n P");
+
+// window.alert (piz);
+
+// Question #6
+// var mail= ("sualehmsiddiqui@gmail.com");
+
+// window.alert ("My Email Address is "+mail);
+
+// Question #7
+// var book = ( ' “A smarter way to learn JavaScript” ' ) ;
+
+// window.alert ("I am to trying learn from the Book "+book);
+
+// Question #8
+// var htmlTxt= ("Yah! I can write HTML through JavaScript")
+
+// document.write (htmlTxt);
+
+// Question #9
+// var string = ('“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”');
+
+// window.alert (string);
